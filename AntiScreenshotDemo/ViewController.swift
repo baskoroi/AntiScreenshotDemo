f75526dae4153f7fe131a0b9d4b459fa1cc82dca
Baskoro Indrayana <baskoroindrayana@gmail.com>
2021-02-10 20:48:49 +0700

    Initial commit

--- AntiScreenshotDemo/ViewController.swift
+++ AntiScreenshotDemo/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
+    private let secondsToAlert: Double = 0.75
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(onScreenshotTriggered),
+            name: UIApplication.userDidTakeScreenshotNotification,
+            object: nil
+        )
     }
-
-
+    
+    @objc
+    private func onScreenshotTriggered() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + secondsToAlert,
+                                      execute: deleteRecentScreenshot)
+    }
+    
+    private func deleteRecentScreenshot() {
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.predicate = NSPredicate(
+            format: "(mediaSubtypes & %d) != 0",
+            PHAssetMediaSubtype.photoScreenshot.rawValue
+        )
+        fetchOptions.sortDescriptors = [
+            NSSortDescriptor(key: "creationDate", ascending: false)
+        ]
+        fetchOptions.fetchLimit = 1
+        
+        let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        if let asset = fetchResult.firstObject {
+            PHPhotoLibrary.shared().performChanges {
+                PHAssetChangeRequest.deleteAssets([asset as Any] as NSArray)
+            }
+        }
+    }
 }
-
